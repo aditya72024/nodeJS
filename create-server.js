31989c8f6c218b20ccaec18e3d131d78ea76bd75
Aditya Ojha <aditya72024@gmail.com>
2023-08-18 19:02:15 +0530

    create-server-with-req-funct...

--- create-server.js
+++ create-server.js
@@ -1,7 +1,19 @@
 const http = require('http');
 
 const server = http.createServer((req,res)=>{
-    console.log('Aditya');
+    res.setHeader('Content-Type','text/html');
+    if(req.url == '/' || req.url == '/home' ){
+        res.write('<html><head><title>Hello There</title></head><body>Welcome to home!!</body></html>')
+
+    }else if(req.url == '/about'){
+        res.write('<html><head><title>Hello There</title></head><body>Welcome to about!!</body></html>')
+
+    }else if(req.url == '/node'){
+        res.write('<html><head><title>Hello There</title></head><body>Welcome to nodejs project!!</body></html>')
+
+    }
+    
+    res.end();
 })
 
 server.listen(4000);
